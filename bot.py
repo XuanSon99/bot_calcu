5a2cb7dd36ebc29a77a29d483bb4dcfd82bd24a1
Your Name <you@example.com>
2023-05-24 15:36:34 +0700

    change

--- bot.py
+++ bot.py
@@ -19,19 +19,34 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     username = update.effective_user.username
     chat_id = update.effective_chat.id
-    
+
     try:
         text = update.message.text
-        msg = f'{text} = {eval(text.replace(",","")):,}'  
+
+        if "+" in text:
+            arr = text.split("+")
+            cal = "+"
+        if "-" in text:
+            arr = text.split("-")
+            cal = "-"
+        if "*" in text:
+            arr = text.split("*")
+            cal = "*"
+        if "/" in text:
+            arr = text.split("/")
+            cal = "/"
+
+        first_num = int(arr[0].replace(",","").strip())
+        last_num = int(arr[1].replace(",","").strip())
+
+        msg = f'{first_num:,} {cal} {last_num:,} = {eval(text.replace(",","")):,}'
+
         await context.bot.send_message(chat_id, text=msg, parse_mode=constants.ParseMode.HTML)
     except:
         print("An exception occurred")
 
-    
-
-
 app = ApplicationBuilder().token(
-    "5974270430:AAHbYv4dSgycHWrL4Q5q8k_P2vigIa89kig").build()
+    "6217705988:AAEOYp5g31rkl-iWrXAGE_mo7t0f0Oz3qIo").build()
 
 app.add_handler(CommandHandler("start", start)) 
 app.add_handler(MessageHandler(filters.ALL, messageHandler))
