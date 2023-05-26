f8753a83b33b978df36eade8ba0e546676a24bfd
Your Name <you@example.com>
2023-05-26 15:11:57 +0700

    change

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from telegram import *
 from telegram.ext import *
+from ast import literal_eval
 
 qr = "👨‍💻 Tạo mã QR"
 
@@ -20,33 +21,37 @@ async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     username = update.effective_user.username
     chat_id = update.effective_chat.id
 
-    try:
-        text = update.message.text
+    # try:
+    text = update.message.text
 
-        if "+" in text:
-            arr = text.split("+")
-            cal = "+"
-        if "-" in text:
-            arr = text.split("-")
-            cal = "-"
-        if "*" in text:
-            arr = text.split("*")
-            cal = "*"
-        if "/" in text:
-            arr = text.split("/")
-            cal = "/"
+    if "+" in text:
+        arr = text.split("+")
+        cal = "+"
+    if "-" in text:
+        arr = text.split("-")
+        cal = "-"
+    if "*" in text:
+        arr = text.split("*")
+        cal = "*"
+    if "/" in text:
+        arr = text.split("/")
+        cal = "/"
 
-        first_num = int(arr[0].replace(",","").strip())
-        last_num = int(arr[1].replace(",","").strip())
+    first_num = literal_eval(arr[0].replace(",","").strip())
+    last_num = literal_eval(arr[1].replace(",","").strip())
+    result = eval(text.replace(",",""))
 
-        msg = f'{first_num:,} {cal} {last_num:,} = {eval(text.replace(",","")):,}'
+    msg = f'{first_num:,} {cal} {last_num:,} = {result:,}'
 
-        await context.bot.send_message(chat_id, text=msg, parse_mode=constants.ParseMode.HTML)
-    except:
-        print("An exception occurred")
+    await context.bot.send_message(chat_id, text=msg, parse_mode=constants.ParseMode.HTML)
+    # except:
+    #     print("An exception occurred")
+
+# app = ApplicationBuilder().token(
+#     "5974270430:AAHbYv4dSgycHWrL4Q5q8k_P2vigIa89kig").build()
 
 app = ApplicationBuilder().token(
-    "5974270430:AAHbYv4dSgycHWrL4Q5q8k_P2vigIa89kig").build()
+    "6217705988:AAEOYp5g31rkl-iWrXAGE_mo7t0f0Oz3qIo").build()
 
 app.add_handler(CommandHandler("start", start)) 
 app.add_handler(MessageHandler(filters.ALL, messageHandler))
