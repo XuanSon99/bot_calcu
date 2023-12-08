c675b3e4204b1b9e3509ca3543f314470b8d9118
Your Name <you@example.com>
2023-12-08 15:47:21 +0700

    chnage

--- bot.py
+++ bot.py
@@ -21,31 +21,35 @@ async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     username = update.effective_user.username
     chat_id = update.effective_chat.id
 
-    # try:
-    text = update.message.text
+    try:
+        text = update.message.text
 
-    if "+" in text:
-        arr = text.split("+")
-        cal = "+"
-    if "-" in text:
-        arr = text.split("-")
-        cal = "-"
-    if "*" in text:
-        arr = text.split("*")
-        cal = "*"
-    if "/" in text:
-        arr = text.split("/")
-        cal = "/"
+        if "+" in text:
+            arr = text.split("+")
+            cal = "+"
+        if "-" in text:
+            arr = text.split("-")
+            cal = "-"
+        if "*" in text:
+            arr = text.split("*")
+            cal = "*"
+        if "/" in text:
+            arr = text.split("/")
+            cal = "/"
 
-    first_num = literal_eval(arr[0].replace(",","").strip())
-    last_num = literal_eval(arr[1].replace(",","").strip())
-    result = eval(text.replace(",",""))
+        first_num = literal_eval(arr[0].replace(",","").strip())
+        last_num = literal_eval(arr[1].replace(",","").strip())
+        result = eval(text.replace(",",""))
 
-    msg = f'{first_num:,} {cal} {last_num:,} = {result:,}'
+        msg = f'{first_num:,} {cal} {last_num:,} = {result:,}'
 
-    await context.bot.send_message(chat_id, text=msg, parse_mode=constants.ParseMode.HTML)
-    # except:
-    #     print("An exception occurred")
+        await context.bot.send_message(chat_id, text=msg, parse_mode=constants.ParseMode.HTML)
+    except:
+        result = eval(text.replace(",",""))
+
+        msg = f'{text} = {result:,}'
+
+        await context.bot.send_message(chat_id, text=msg, parse_mode=constants.ParseMode.HTML)
 
 app = ApplicationBuilder().token(
     "5974270430:AAHbYv4dSgycHWrL4Q5q8k_P2vigIa89kig").build()
